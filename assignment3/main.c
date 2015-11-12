c1bd2bf277dbfbaa9ae4e0d94b68a36766758a5e
cla235 <cla235@asb9700u-d07.csil.sfu.ca>
2015-11-12 14:21:54 -0800

    modified assignment 3 to do parent stuff

--- assignment3/main.c
+++ assignment3/main.c
@@ -39,10 +39,6 @@ int main (int argc, char** argv) {
         return 1;
     }
 
-    // initialize variables for child process to use
-    // and to keep track of child processes
-    char *input_filename, *output_filename, *filenames, *filename_ptr;
-
     // terminate process if child process id array cannot be malloc'd
     if (pid == NULL) {
         current_time = time(NULL);
@@ -56,36 +52,37 @@ int main (int argc, char** argv) {
     // check scheduling algorithm (terminate if invalid input)
     int method = 0;
 
-    filenames = malloc(sizeof(char)*20);
-    memset(filenames, 0, sizeof(char)*20);
-    fgets(filenames, 20, input);
+    char *method_check = malloc(sizeof(char)*20);
+    memset(method_check, 0, sizeof(char)*20);
+    fgets(method_check, 20, input);
 
-    if (strcmp(filenames, "round robin") == 0) method = 1;
-    else if (strcmp(filenames, "fcfs") == 0) method = 2;
+    if (strcmp(method_check, "round robin") == 0) method = 1;
+    else if (strcmp(method_check, "fcfs") == 0) method = 2;
     else {
         current_time = time(NULL);
         time_string = ctime(&current_time);
         time_string[strlen(time_string)-1] = '\0';
         printf("[%s] ERROR in parent process (#%d):\n\tinvalid scheduling method. Terminating.\n", time_string, getpid());
-        free(filenames);
+        free(method_check);
         fclose(input);
         return 3;
     }
+    free(method_check);
 
     // count number of cores available for child processes
-    int cores = sysconf(_SC_NPROCCESSORS_ONLN) - 1;
+    int available_cores = sysconf(_SC_NPROCCESSORS_ONLN) - 1;
 
     // initialize n child processes (n = number of cores)
     // associate children with respective pipes
     int *pid = malloc(sizeof(int)*cores);
-    int pipes[2*cores][2];
+    int pipes[2*available_cores][2];
     bool child_check = false;
     int i, flag;
 
     if (method == 1) flag = 0;
     else flag = O_NONBLOCK;
 
-    for (i = 0; i < cores; i++) {
+    for (i = 0; i < available_cores; i++) {
         // create pipes for communication
         // first pipe is for parent -> child
         // second pipe is for child -> parent (can be non-blocking)
@@ -118,7 +115,7 @@ int main (int argc, char** argv) {
             current_time = time(NULL);
             time_string = ctime(&current_time);
             time_string[strlen(time_string)-1] = '\0';
-            printf("[%s] Child process ID #%d was created successfully (%d).\n", time_string, getpid(), i);
+            printf("[%s] Child process ID #%d was created successfully (%d).\n", time_string, getpid(), i+1);
             close(pipes[2*i][1]);
             close(pipes[2*i+1][0]);
             child_check = true;
@@ -133,6 +130,102 @@ int main (int argc, char** argv) {
 
     // check if parent or child, branch accordingly
     // parent should check for failed fork() calls before continuing
+
+    // child process
+    if (child_check) {
+
+    }
+
+    // parent process
+    else {
+    	// initialize variables for child process to use
+    	char *input_filename, *output_filename, *filenames, *filename_ptr;
+
+    	// round robin
+    	if (method == 1) {
+    		while(!feof(input)) {
+    			// setup for extracting input and output filenames from one line
+    			filenames = malloc(sizeof(char)*2100);
+    			memset(filenames, 0, sizeof(char)*2100);
+    			filename_ptr = filenames;
+
+    			// split "filenames" string to input and output components
+    			fgets(filenames, 2100, input);
+    			input_filename = strsep(&filenames, " ");
+    			output_filename = strsep(&filenames, "\n");
+
+    			// skip the line if no input or output was supplied
+    			if (input_filename == NULL || output_filename == NULL) {
+		            current_time = time(NULL);
+		            time_string = ctime(&current_time);
+		            time_string[strlen(time_string)-1] = '\0';
+		            printf("[%s] WARNING in parent process (#%d):\n\t\"input_filename\" or \"output_filename\" is NULL. Skipping.\n", time_string, getpid());
+		            
+		            free(filename_ptr);
+
+		            continue;
+        		}
+
+        		// skip the line if input and output are the same
+        		else if (strcmp(input_filename, output_filename) == 0) {
+		            current_time = time(NULL);
+		            time_string = ctime(&current_time);
+		            time_string[strlen(time_string)-1] = '\0';
+		            printf("[%s] WARNING in parent process (#%d):\n\tfound an instance of \"input_filename\" = \"output_filename\". Skipping.\n", time_string, getpid());
+		            
+		            free(filename_ptr);
+
+		            continue;
+		        }
+
+		        // send input_filename and output_filename to next child process
+
+    		}
+    	}
+
+    	// fcfs
+    	else {
+    		while(!feof(input)) {
+				// setup for extracting input and output filenames from one line
+    			filenames = malloc(sizeof(char)*2100);
+    			memset(filenames, 0, sizeof(char)*2100);
+    			filename_ptr = filenames;
+
+    			// split "filenames" string to input and output components
+    			fgets(filenames, 2100, input);
+    			input_filename = strsep(&filenames, " ");
+    			output_filename = strsep(&filenames, "\n");
+
+    			// skip the line if no input or output was supplied
+    			if (input_filename == NULL || output_filename == NULL) {
+		            current_time = time(NULL);
+		            time_string = ctime(&current_time);
+		            time_string[strlen(time_string)-1] = '\0';
+		            printf("[%s] WARNING in parent process (#%d):\n\t\"input_filename\" or \"output_filename\" is NULL. Skipping.\n", time_string, getpid());
+		            
+		            free(filename_ptr);
+
+		            continue;
+        		}
+
+        		// skip the line if input and output are the same
+        		else if (strcmp(input_filename, output_filename) == 0) {
+		            current_time = time(NULL);
+		            time_string = ctime(&current_time);
+		            time_string[strlen(time_string)-1] = '\0';
+		            printf("[%s] WARNING in parent process (#%d):\n\tfound an instance of \"input_filename\" = \"output_filename\". Skipping.\n", time_string, getpid());
+		            
+		            free(filename_ptr);
+
+		            continue;
+		        }
+
+		        // send input_filename and output_filename to next child process
+		        
+		    }
+    	}
+
+    }
 
     return 0;
 }
